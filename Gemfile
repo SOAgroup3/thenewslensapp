c62345ae57dd115be2fbcf4c38c0a31fdad8020e
peyruchao <happypeggy8138@gmail.com>
2014-11-07 23:20:31 +0800

    final merge

--- Gemfile
+++ Gemfile
@@ -3,3 +3,10 @@ source 'http://rubygems.org'
 gem 'sinatra'
 gem 'thenewslensapi'
 gem 'json'
+
+group :test do
+  gem 'minitest'
+  gem 'rack'
+  gem 'rack-test'
+  gem 'rake'
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -24,4 +24,34 @@ describe 'Thenewlensapp Stories' do
       last_response.must_be :not_found?
     end
   end
+
+  describe 'Checking users for badges' do
+    it 'should find missing badges' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = {
+        col_name:['title']
+      }
+
+      post '/api/v1/specify', body.to_json, header
+      last_response.must_be :ok?
+    end
+
+    it 'should return 404 for unknown users' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = {
+        col_name: [random_str(15)]
+      }
+
+      post '/api/v1/specify', body.to_json, header
+      last_response.must_be :not_found?
+    end
+
+    it 'should return 400 for bad JSON formatting' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = random_str(50)
+
+      post '/api/v1/specify', body, header
+      last_response.must_be :bad_request?
+    end
+  end
 end
