3a52c82ece091e770d1f0b44b15b3b67046d0f15
LinAnita <a412262001@gmail.com>
2014-11-16 19:36:40 +0800

    database v2

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
 require 'thenewslensapi'
 require 'json'
+require './academy'
 
 # Simple version of nba_scrapper
 class ThenewslensApp < Sinatra::Base
+
+  configure :production, :development do
+    enable :logging
+  end
+
 	helpers do
     def get_news(number)
       begin
@@ -53,5 +59,41 @@ class ThenewslensApp < Sinatra::Base
       rescue
         halt 404
       end
-  end
+    end
+
+
+    post '/api/v1/tutorials' do
+      content_type :json
+      begin
+        req = JSON.parse(request.body.read)
+        logger.info req
+      rescue
+        halt 400
+      end
+
+      tutorial = Tutorial.new
+      tutorial.title = req['title'].to_json
+      tutorial.author = req['author'].to_json
+      tutorial.date = req['date'].to_json
+
+      if tutorial.save
+        status 201
+        redirect "/api/v1/tutorials/#{tutorial.id}"
+      end
+    end
+
+    get '/api/v1/tutorials/:id' do
+      content_type :json
+      begin
+        @tutorial = Tutorial.find(params[:id])
+        title = JSON.parse(@tutorial.title)
+        author = JSON.parse(@tutorial.author)
+        date = JSON.parse(@tutorial.date)
+        logger.info({ title: title, author: author, date: date }.to_json)
+      rescue
+        halt 400
+      end
+      
+      get_news(:id).to_json
+    end
 end
