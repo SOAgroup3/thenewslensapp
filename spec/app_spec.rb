67b9c738c5209efd40a75ed34cfdeaa96356d443
LinAnita <a412262001@gmail.com>
2014-11-30 22:09:29 +0800

    add database v1

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -16,51 +16,30 @@ describe 'Thenewlensapp Stories' do
 
   describe 'Getting news information' do
     it 'should return news' do
-      get '/api/v1/5.json'
+      get '/api/v1/3.json'
       last_response.must_be :ok?
     end
 
-    #it 'should return 404 for not a specific number' do
-    #  get "/api/v1/#{random_str(20)}.json"
-    #  last_response.must_be :not_found?
-    #end
-  end
-=begin
-  describe 'Checking for columns' do
-    #
-    #it 'should find date' do
-    #  header = { 'CONTENT_TYPE' => 'application/json' }
-    #  body = {
-    #    col_name: ['date']
-    #  }
-
-    #  post '/api/v1/specify.json', body.to_json, header
-    #  last_response.must_be :ok?
-    #end
-    before do
-      Tutorial.delete_all
+    it 'should return 404 for not a specific number' do
+      get "/api/v1/#{random_str(20)}.json"
+      last_response.must_be :not_found?
     end
+  end
 
-    it 'should find missing column' do
+
+  describe 'Checking for post col_name' do
+    it 'should return ok for exist column name' do
       header = { 'CONTENT_TYPE' => 'application/json' }
       body = {
-        number: '3'
+        col_name: ['title']
       }
+      post '/api/v1/specify.json', body.to_json, header
+      last_response.must_be :ok?
+    end
 
-      # Check redirect URL from post request
-      post '/api/v1/tutorials', body.to_json, header
-      last_response.must_be :redirect?
-      next_location = last_response.location
-      next_location.must_match /api\/v2\/tutorials\/\d+/
-
-      # Check if request parameters are stored in ActiveRecord data store
-      tut_id = next_location.scan(/tutorials\/(\d+)/).flatten[0].to_i
-      save_tutorial = Tutorial.find(tut_id)
-      JSON.parse(save_tutorial[:date]).must_equal body[:date]
-
-      # Check if redirect works
-      follow_redirect!
-      last_request.url.must_match /api\/v2\/tutorials\/\d+/
+    it 'should return 404 for empty post body' do
+      post '/api/v1/specify.json', {}
+      last_response.must_be :not_found?
     end
 
     it 'should return 404 for unknown column name' do
@@ -68,7 +47,6 @@ describe 'Thenewlensapp Stories' do
       body = {
         col_name: [random_str(15)]
       }
-
       post '/api/v1/specify.json', body.to_json, header
       last_response.must_be :not_found?
     end
@@ -79,8 +57,16 @@ describe 'Thenewlensapp Stories' do
 
       post '/api/v1/specify.json', body, header
       last_response.must_be :not_found?
-
     end
   end
-=end  
+
+  describe 'Checking for database operation' do   
+    #before test database type 
+    # rake db:migrate RACK_ENV=test
+    #(therefore create a new database for testing )
+    before do
+      Tutorial.delete_all
+    end
+  end
+
 end
